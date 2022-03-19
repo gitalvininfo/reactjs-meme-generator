51236900d2b2f503a5b35bbaf99f918e7d7129cc
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-20 00:08:52 +0800

    done in forms

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
 
 import React from "react";
-import Header from "./components/Header";
 import './style.css';
+import Header from "./components/Header";
+import Meme from "./components/Meme";
+
 
 export default function App() {
     return (
-        <Header />
+        <div>
+            <Header />
+            <Meme />
+        </div>
     )
 }
