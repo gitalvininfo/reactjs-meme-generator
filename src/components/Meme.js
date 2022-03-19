6b0393ffd9d6bb300b39de6a69b63cd4c3fbfb28
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-20 01:57:40 +0800

    added image to generator

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import memesData from '../memesData';
 
 
 export default function Meme() {
+
+    const [memeImage, setMemeImage] = useState("");
 
 
     function handleClick() {
         const memes = memesData.data.memes;
         let item = memes[Math.floor(Math.random() * memes.length)];
         console.log(item.url);
+
+        setMemeImage(item.url);
     }
 
     return (
@@ -18,6 +22,9 @@ export default function Meme() {
                 <input type="text" className="form--input" placeholder="Bottom Text" />
                 <button onClick={handleClick} className="form--button" type="button">Get a new meme image</button>
             </div>
+
+            <img src={memeImage} className="meme--img" />
+
         </main>
     )
 }
