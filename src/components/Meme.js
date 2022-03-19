e51376583a58ffb2a30a07325be0cfc30d17513c
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-20 00:39:45 +0800

    done in getting a meme randomly

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,23 @@
 import React from "react";
+import memesData from '../memesData';
+
 
 export default function Meme() {
-    return(
+
+
+    function handleClick() {
+        const memes = memesData.data.memes;
+        let item = memes[Math.floor(Math.random() * memes.length)];
+        console.log(item.url);
+    }
+
+    return (
         <main>
-            <form className="form">
-                <input type="text" className="form--input" placeholder="Top Text"/>
-                <input type="text" className="form--input" placeholder="Bottom Text"/>
-                <button className="form--button">Get a new meme image</button>
-            </form>
+            <div className="form">
+                <input type="text" className="form--input" placeholder="Top Text" />
+                <input type="text" className="form--input" placeholder="Bottom Text" />
+                <button onClick={handleClick} className="form--button" type="button">Get a new meme image</button>
+            </div>
         </main>
     )
 }
